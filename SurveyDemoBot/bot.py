3bcff34256dc83322e9c2f7dd3a7b19982ffad8a
Alexey Lobzov <aleks.lobzov@gmail.com>
2017-01-04 13:58:40 +0300

    Multiuser answer support added

--- SurveyDemoBot/bot.py
+++ SurveyDemoBot/bot.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import config
+import os
 import telebot
 from telebot import types
 
@@ -26,24 +27,33 @@ for r in f:
 
 @bot.message_handler(content_types=["text"])
 def answer_the_question(message):
-    f = open(config.path + 'Answers.txt', 'r')
+
+    # формируем путь к файлу с ответами пользователя
+    file_name = config.path + str(message.chat.id) + '_Answers.txt'
+
+    # создаем файл с ответами в случае его отсутствия
+    if os.path.exists(file_name):
+        f = open(file_name, 'r')
+    else:
+        f = open(file_name, 'w+')
     file_length = len(f.readlines())
     f.close()
 
+    # формируем сообщение для пользователя
     if message.text == '/start start' or message.text == '/start':
         test_question = questionnaire[0]
-        f = open(config.path + 'Answers.txt', 'w')
+        f = open(file_name, 'w')
         f.close()
     elif message.text[0] not in ['1','2','3','4','5']:
         test_question = "Выбирете один из доступных ответов"
     elif file_length == 0:
-        f = open(config.path + 'Answers.txt', 'a')
+        f = open(file_name, 'a')
         f.write(str(file_length) + ' - ' + message.text[0])
         test_question = questionnaire[file_length + 1]
     elif file_length == len(questionnaire):
         test_question = "Опрос завершен. Спасибо за участие!"
     else:
-        f = open(config.path + 'Answers.txt', 'a')
+        f = open(file_name, 'a')
         f.write('\n')
         f.write(str(file_length) + ' - ' + message.text[0])
         if file_length + 1 == len(questionnaire):
@@ -51,6 +61,8 @@ def answer_the_question(message):
         else:
             test_question = questionnaire[file_length + 1]
         f.close()
+
+    # отправляем сообщение пользователю
     bot.send_message(message.chat.id, test_question, reply_markup=markup)
 
 
